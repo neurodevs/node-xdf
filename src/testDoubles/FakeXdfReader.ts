ae191ce8fc49b3d2421116b0426f683b072e4c74
eric-yates <hello@ericthecurious.com>
2024-10-24 19:43:35 -0600

    patch: add numConstructorCalls

--- src/testDoubles/FakeXdfReader.ts
+++ src/testDoubles/FakeXdfReader.ts
@@ -2,8 +2,13 @@ import { XdfFile } from '../Libxdf'
 import { XdfReader, XdfReaderLoadOptions } from '../XdfReader'
 
 export default class FakeXdfReader implements XdfReader {
+	public static numCallsToConstructor = 0
 	public static callsToLoad: FakeLoadCall[] = []
 	public static fakeResponse: XdfFile = {} as XdfFile
+
+	public constructor() {
+		FakeXdfReader.numCallsToConstructor++
+	}
 
 	public async load(filePath: string, options?: XdfReaderLoadOptions) {
 		this.callsToLoad.push({ filePath, options })
