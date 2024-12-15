0b74aed733d0a5a021c2a47ca2a5985afb0651c2
ericthecurious <hello@ericthecurious.com>
2024-12-14 21:20:02 -0700

    patch: calls ffi-rs open with required options

--- src/__tests__/LabrecorderAdapter.test.ts
+++ src/__tests__/LabrecorderAdapter.test.ts
@@ -4,15 +4,20 @@ import AbstractSpruceTest, {
     errorAssert,
     generateId,
 } from '@sprucelabs/test-utils'
+import { OpenParams } from 'ffi-rs'
 import LabrecorderAdapter, {
     Labrecorder,
 } from '../components/LabrecorderAdapter'
 
 export default class LabrecorderAdapterTest extends AbstractSpruceTest {
     private static instance: Labrecorder
+    private static ffiRsOpenOptions?: OpenParams
 
     protected static async beforeEach() {
         await super.beforeEach()
+
+        this.fakeFfiRsOpen()
+
         this.instance = this.LabrecorderAdapter()
     }
 
@@ -33,7 +38,21 @@ export default class LabrecorderAdapterTest extends AbstractSpruceTest {
         })
     }
 
+    @test()
+    protected static async callsFfiRsOpenWithRequiredOptions() {
+        assert.isEqualDeep(this.ffiRsOpenOptions, {
+            library: 'labrecorder',
+            path: this.labrecorderPath,
+        })
+    }
+
     private static readonly labrecorderPath = generateId()
+
+    private static fakeFfiRsOpen() {
+        LabrecorderAdapter.ffiRsOpen = (options) => {
+            this.ffiRsOpenOptions = options
+        }
+    }
 
     private static LabrecorderAdapter(labrecorderPath?: string) {
         return LabrecorderAdapter.Create(
--- src/components/LabrecorderAdapter.ts
+++ src/components/LabrecorderAdapter.ts
@@ -1,13 +1,28 @@
 import { assertOptions } from '@sprucelabs/schema'
+import { open } from 'ffi-rs'
 
 export default class LabrecorderAdapter implements Labrecorder {
     public static Class?: LabrecorderAdapterConstructor
+    public static ffiRsOpen = open
 
-    protected constructor() {}
+    private labrecorderPath: string
+
+    protected constructor(labrecorderPath: string) {
+        this.labrecorderPath = labrecorderPath
+
+        this.ffiRsOpen({
+            library: 'labrecorder',
+            path: this.labrecorderPath,
+        })
+    }
 
     public static async Create(labrecorderPath: string) {
         assertOptions({ labrecorderPath }, ['labrecorderPath'])
-        return new (this.Class ?? this)()
+        return new (this.Class ?? this)(labrecorderPath)
+    }
+
+    private get ffiRsOpen() {
+        return LabrecorderAdapter.ffiRsOpen
     }
 }
 
