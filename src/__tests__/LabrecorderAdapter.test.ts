bc03572a053fc5f324fa4dd96f34f06fe2342558
ericthecurious <hello@ericthecurious.com>
2024-12-15 19:23:49 -0700

    patch: calls ffi-rs define with required options

--- src/__tests__/LabrecorderAdapter.test.ts
+++ src/__tests__/LabrecorderAdapter.test.ts
@@ -4,7 +4,7 @@ import AbstractSpruceTest, {
     errorAssert,
     generateId,
 } from '@sprucelabs/test-utils'
-import { OpenParams } from 'ffi-rs'
+import { DataType, OpenParams } from 'ffi-rs'
 import LabrecorderAdapter, {
     Labrecorder,
 } from '../components/LabrecorderAdapter'
@@ -12,11 +12,13 @@ import LabrecorderAdapter, {
 export default class LabrecorderAdapterTest extends AbstractSpruceTest {
     private static instance: Labrecorder
     private static ffiRsOpenOptions?: OpenParams
+    private static ffiRsDefineOptions?: Record<string, any>
 
     protected static async beforeEach() {
         await super.beforeEach()
 
         this.fakeFfiRsOpen()
+        this.fakeFfiRsDefine()
 
         this.instance = this.LabrecorderAdapter()
     }
@@ -46,13 +48,57 @@ export default class LabrecorderAdapterTest extends AbstractSpruceTest {
         })
     }
 
-    private static readonly labrecorderPath = generateId()
+    @test()
+    protected static async callsFfiRsDefineWithRequiredOptions() {
+        await this.LabrecorderAdapter()
+
+        assert.isEqualDeep(
+            this.ffiRsDefineOptions,
+            {
+                recording_create: {
+                    library: 'labrecorder',
+                    retType: DataType.External, // Pointer to the recording object
+                    paramsType: [
+                        DataType.String, //   filename
+                        DataType.External, // streams
+                        DataType.External, // watchfor
+                        DataType.External, // syncOptions
+                        DataType.Boolean, //  `collect_offsets
+                    ],
+                },
+                recording_stop: {
+                    library: 'labrecorder',
+                    retType: DataType.Void,
+                    paramsType: [
+                        DataType.External, // Pointer to the recording object
+                    ],
+                },
+                recording_delete: {
+                    library: 'labrecorder',
+                    retType: DataType.Void,
+                    paramsType: [
+                        DataType.External, // Pointer to the recording object
+                    ],
+                },
+            },
+            'Please pass valid options to ffiRsDefine!'
+        )
+    }
 
     private static fakeFfiRsOpen() {
         LabrecorderAdapter.ffiRsOpen = (options) => {
             this.ffiRsOpenOptions = options
         }
     }
+
+    private static fakeFfiRsDefine() {
+        LabrecorderAdapter.ffiRsDefine = (options) => {
+            this.ffiRsDefineOptions = options
+            return {} as any
+        }
+    }
+
+    private static readonly labrecorderPath = generateId()
 
     private static LabrecorderAdapter(labrecorderPath?: string) {
         return LabrecorderAdapter.Create(
--- src/components/LabrecorderAdapter.ts
+++ src/components/LabrecorderAdapter.ts
@@ -1,9 +1,10 @@
 import { assertOptions } from '@sprucelabs/schema'
-import { open } from 'ffi-rs'
+import { DataType, define, open } from 'ffi-rs'
 
 export default class LabrecorderAdapter implements Labrecorder {
     public static Class?: LabrecorderAdapterConstructor
     public static ffiRsOpen = open
+    public static ffiRsDefine = define
 
     private labrecorderPath: string
 
@@ -14,6 +15,34 @@ export default class LabrecorderAdapter implements Labrecorder {
             library: 'labrecorder',
             path: this.labrecorderPath,
         })
+
+        this.ffiRsDefine({
+            recording_create: {
+                library: 'labrecorder',
+                retType: DataType.External, // Pointer to the recording object
+                paramsType: [
+                    DataType.String, //    filename
+                    DataType.External, //  streams
+                    DataType.External, //  watchfor
+                    DataType.External, //  syncOptions
+                    DataType.Boolean, //   collect_offsets
+                ],
+            },
+            recording_stop: {
+                library: 'labrecorder',
+                retType: DataType.Void,
+                paramsType: [
+                    DataType.External, // Pointer to the recording object
+                ],
+            },
+            recording_delete: {
+                library: 'labrecorder',
+                retType: DataType.Void,
+                paramsType: [
+                    DataType.External, // Pointer to the recording object
+                ],
+            },
+        })
     }
 
     public static async Create(labrecorderPath: string) {
@@ -23,6 +52,10 @@ export default class LabrecorderAdapter implements Labrecorder {
 
     private get ffiRsOpen() {
         return LabrecorderAdapter.ffiRsOpen
+    }
+
+    private get ffiRsDefine() {
+        return LabrecorderAdapter.ffiRsDefine
     }
 }
 
