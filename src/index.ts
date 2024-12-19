75d4705b831723ddd1e74b4e3bc33194849e92a4
ericthecurious <hello@ericthecurious.com>
2024-12-19 01:44:34 -0600

    patch: fix and clean up exports

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
+export { default as LabrecorderAdapter } from './components/LabrecorderAdapter'
+export * from './components/LabrecorderAdapter'
+
 export { default as LibxdfAdapter } from './components/LibxdfAdapter'
 export * from './components/LibxdfAdapter'
 
-export { default as SpyLibxdf } from './testDoubles/Libxdf/SpyLibxdf'
-export * from './testDoubles/Libxdf/SpyLibxdf'
-
-export { default as FakeLibxdf } from './testDoubles/Libxdf/FakeLibxdf'
-export * from './testDoubles/Libxdf/FakeLibxdf'
-
 export { default as XdfFileLoader } from './components/XdfFileLoader'
 export * from './components/XdfFileLoader'
-
-export { default as SpyXdfLoader } from './testDoubles/XdfLoader/SpyXdfLoader'
-export * from './testDoubles/XdfLoader/SpyXdfLoader'
-
-export { default as FakeXdfLoader } from './testDoubles/XdfLoader/FakeXdfLoader'
-export * from './testDoubles/XdfLoader/FakeXdfLoader'
 
 export { default as XdfStreamRecorder } from './components/XdfStreamRecorder'
 export * from './components/XdfStreamRecorder'
 
 export { default as XdfStreamReplayer } from './components/XdfStreamReplayer'
 export * from './components/XdfStreamReplayer'
+
+export { default as FakeLabrecorder } from './testDoubles/Labrecorder/FakeLabrecorder'
+export * from './testDoubles/Labrecorder/FakeLabrecorder'
+
+export { default as FakeLibxdf } from './testDoubles/Libxdf/FakeLibxdf'
+export * from './testDoubles/Libxdf/FakeLibxdf'
+
+export { default as FakeXdfLoader } from './testDoubles/XdfLoader/FakeXdfLoader'
+export * from './testDoubles/XdfLoader/FakeXdfLoader'
+
+export { default as SpyLibxdf } from './testDoubles/Libxdf/SpyLibxdf'
+export * from './testDoubles/Libxdf/SpyLibxdf'
+
+export { default as SpyXdfLoader } from './testDoubles/XdfLoader/SpyXdfLoader'
+export * from './testDoubles/XdfLoader/SpyXdfLoader'
