5bf7fd9ccf7c1746d46fd43c966a68fc428ef886
ericthecurious <hello@ericthecurious.com>
2025-09-01 16:42:35 -0700

    patch: add comments to organize exports by interface

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,45 @@
+// Labrecorder
+
 export { default as LabrecorderAdapter } from './modules/LabrecorderAdapter'
 export * from './modules/LabrecorderAdapter'
-
-export { default as LibxdfAdapter } from './modules/LibxdfAdapter'
-export * from './modules/LibxdfAdapter'
-
-export { default as XdfFileLoader } from './modules/XdfFileLoader'
-export * from './modules/XdfFileLoader'
-
-export { default as XdfStreamRecorder } from './modules/XdfStreamRecorder'
-export * from './modules/XdfStreamRecorder'
-
-export { default as XdfStreamReplayer } from './modules/XdfStreamReplayer'
-export * from './modules/XdfStreamReplayer'
 
 export { default as FakeLabrecorder } from './testDoubles/Labrecorder/FakeLabrecorder'
 export * from './testDoubles/Labrecorder/FakeLabrecorder'
 
+// Libxdf
+
+export { default as LibxdfAdapter } from './modules/LibxdfAdapter'
+export * from './modules/LibxdfAdapter'
+
 export { default as FakeLibxdf } from './testDoubles/Libxdf/FakeLibxdf'
 export * from './testDoubles/Libxdf/FakeLibxdf'
-
-export { default as FakeXdfLoader } from './testDoubles/XdfLoader/FakeXdfLoader'
-export * from './testDoubles/XdfLoader/FakeXdfLoader'
-
-export { default as FakeXdfRecorder } from './testDoubles/XdfRecorder/FakeXdfRecorder'
-export * from './testDoubles/XdfRecorder/FakeXdfRecorder'
 
 export { default as SpyLibxdf } from './testDoubles/Libxdf/SpyLibxdf'
 export * from './testDoubles/Libxdf/SpyLibxdf'
 
+// XdfFileLoader
+
+export { default as XdfFileLoader } from './modules/XdfFileLoader'
+export * from './modules/XdfFileLoader'
+
+export { default as FakeXdfLoader } from './testDoubles/XdfLoader/FakeXdfLoader'
+export * from './testDoubles/XdfLoader/FakeXdfLoader'
+
 export { default as SpyXdfLoader } from './testDoubles/XdfLoader/SpyXdfLoader'
 export * from './testDoubles/XdfLoader/SpyXdfLoader'
 
+// XdfStreamRecorder
+
+export { default as XdfStreamRecorder } from './modules/XdfStreamRecorder'
+export * from './modules/XdfStreamRecorder'
+
+export { default as FakeXdfRecorder } from './testDoubles/XdfRecorder/FakeXdfRecorder'
+export * from './testDoubles/XdfRecorder/FakeXdfRecorder'
+
 export { default as SpyXdfRecorder } from './testDoubles/XdfRecorder/SpyXdfRecorder'
 export * from './testDoubles/XdfRecorder/SpyXdfRecorder'
+
+// XdfStreamReplayer
+
+export { default as XdfStreamReplayer } from './modules/XdfStreamReplayer'
+export * from './modules/XdfStreamReplayer'
