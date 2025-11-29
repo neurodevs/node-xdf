d03bf2d727219ceb42c167cbe4efdefdfbb87da3
ericthecurious <hello@ericthecurious.com>
2025-11-28 16:41:31 -0800

    patch: refactor for glanceability

--- src/impl/LabrecorderAdapter.ts
+++ src/impl/LabrecorderAdapter.ts
@@ -6,16 +6,13 @@ export default class LabrecorderAdapter implements Labrecorder {
     public static ffiRsDefine = define
 
     private labrecorderPath: string
-    private bindings!: LabrecorderBindings
-    private filename!: string
-    private watchFor!: string[]
-    private recording!: BoundRecording
+    private bindings: LabrecorderBindings
 
     protected constructor(labrecorderPath: string) {
         this.labrecorderPath = labrecorderPath
+        this.loadCppLibrary()
 
-        this.loadLibrary()
-        this.registerFunctions()
+        this.bindings = this.defineBindingFunctions()
     }
 
     public static Create(labrecorderPath?: string) {
@@ -23,68 +20,48 @@ export default class LabrecorderAdapter implements Labrecorder {
         return new (this.Class ?? this)(path)
     }
 
-    private loadLibrary() {
-        this.ffiRsOpen(this.libraryOptions)
-    }
-
-    private registerFunctions() {
-        this.bindings = this.ffiRsDefine(this.functions) as LabrecorderBindings
-    }
-
-    public createRecording(filename: string, watchFor: string[]) {
-        this.filename = filename
-        this.watchFor = watchFor
-
-        return this.callRecordingCreate()
-    }
-
-    private callRecordingCreate() {
-        return this.bindings.recording_create([this.filename, this.watchFor])
-    }
-
-    public stopRecording(recording: BoundRecording) {
-        this.recording = recording
-        this.callRecordingStop()
-    }
-
-    private callRecordingStop() {
-        this.bindings.recording_stop([this.recording])
-    }
-
-    public deleteRecording(recording: BoundRecording) {
-        this.stopRecording(recording)
-        this.callRecordingDelete()
-    }
-
-    private callRecordingDelete() {
-        this.bindings.recording_delete([this.recording])
-    }
-
-    private get libraryOptions() {
-        return {
+    private loadCppLibrary() {
+        this.ffiRsOpen({
             library: 'labrecorder',
             path: this.labrecorderPath,
-        }
+        })
+    }
+
+    private defineBindingFunctions() {
+        return this.ffiRsDefine(this.functions) as LabrecorderBindings
     }
 
     private get functions() {
         return {
             recording_create: {
                 library: 'labrecorder',
-                retType: this.BoundRecordingType,
-                paramsType: [this.FilenameType, this.WatchForType],
+                retType: DataType.External,
+                paramsType: [DataType.String, DataType.StringArray],
             },
             recording_stop: {
                 library: 'labrecorder',
                 retType: DataType.Void,
-                paramsType: [this.BoundRecordingType],
+                paramsType: [DataType.External],
             },
             recording_delete: {
                 library: 'labrecorder',
                 retType: DataType.Void,
-                paramsType: [this.BoundRecordingType],
+                paramsType: [DataType.External],
             },
         }
+    }
+
+    public createRecording(filename: string, watchFor: string[]) {
+        return this.bindings.recording_create([filename, watchFor])
+    }
+
+    public stopRecording(recording: BoundRecording) {
+        this.bindings.recording_stop([recording])
+    }
+
+    public deleteRecording(recording: BoundRecording) {
+        this.stopRecording(recording)
+        this.bindings.recording_delete([recording])
     }
 
     private get ffiRsOpen() {
@@ -94,10 +71,6 @@ export default class LabrecorderAdapter implements Labrecorder {
     private get ffiRsDefine() {
         return LabrecorderAdapter.ffiRsDefine
     }
-
-    private readonly FilenameType = DataType.String
-    private readonly WatchForType = DataType.StringArray
-    private readonly BoundRecordingType = DataType.External
 }
 
 export interface Labrecorder {
